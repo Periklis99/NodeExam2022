e35f9c3b2c1d28d86c981f1380974103ac962b8c
Periklis99 <per.tso93@gmail.com>
2022-06-23 19:30:59 +0200

    socket fix

--- server.js
+++ server.js
@@ -10,6 +10,35 @@ const User = require("./models/user");
 const helmet = require("helmet");
 require("dotenv").config();
 
+
+var http = require("http").createServer(app);
+var io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+});
+
+http.listen(3536, function(){
+console.log("Successfully Connected Node Server");
+
+ io.on("connection", function(socket){
+    console.log(socket)
+
+  });
+});
+
+/*const io = require("socket.io")(http, {
+    cors: {
+        origin: "http://localhost:8080",
+        methods: ["GET", "POST"]
+    }
+})
+io.on('connection', client => { "http://localhost:8080"});
+http.listen(3536)*/
+
+
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
@@ -28,14 +57,7 @@ const sessionMiddleware = session({
   })
   app.use(sessionMiddleware);
 
-  const http = require("http");
-const server = http.createServer(app);
 
-const Server = require("socket.io").Server
-const io = new Server(server);
-
-const wrap = middleware => (socket, next) => middleware(socket.request, {}, next);
-io.use(wrap(sessionMiddleware));
 
 
 
@@ -48,6 +70,6 @@ app.use(passport.session());
 const authRouter = require('./routes/auth.js');
 app.use('/api/auth',authRouter);
 
-app.listen(3536, () => {
+/*app.listen(3536, () => {
     console.log("Server started on port 3536");
-});
+});*/
